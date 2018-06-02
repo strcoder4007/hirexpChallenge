af0f9c4c0bc2ab440001f1b6d9c0bf984d0243c9
Shubham Singh <mynameshubham@gmail.com>
2018-06-02 21:04:16 +0400

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, OnChanges } from '@angular/core';
 
 @Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']})
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnChanges {
     title = 'app';
 
     items = [];
     cnt = 0;
+    fontSize = 15;
 
     delete(idx) {
-        --this.cnt;
         this.items.splice(this.items.indexOf(idx)+1, 1);
     }
 
@@ -17,7 +17,13 @@ export class AppComponent implements OnInit {
         this.items.push(this.cnt);
     }
 
+    valueChanged(e) {
+        this.fontSize = e;
+    }
 
+    ngOnChanges() {
+
+    }
 
     ngOnInit() {
         
