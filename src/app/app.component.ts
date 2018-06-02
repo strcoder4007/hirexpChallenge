b5b366538dbc64c2ceec114279f53a160b6c9979
Shubham Singh <mynameshubham@gmail.com>
2018-06-02 20:49:30 +0400

    list done; layout done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
+@Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.css']})
+export class AppComponent implements OnInit {
+    title = 'app';
+
+    items = [];
+    cnt = 0;
+
+    delete(idx) {
+        --this.cnt;
+        this.items.splice(this.items.indexOf(idx)+1, 1);
+    }
+
+    addMore() {
+        ++this.cnt;
+        this.items.push(this.cnt);
+    }
+
+
+
+    ngOnInit() {
+        
+    }
+
 }
